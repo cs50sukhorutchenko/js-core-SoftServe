647a3436a9ff856d2ea78d18276b8c240b0ad082
Volodymyr Sukhorutchenko <cs50sukhorutchenko@gmail.com>
2022-08-03 22:57:27 +0300

    Add task in README

--- hw1/js/area.js
+++ hw1/js/area.js
@@ -1,16 +1,3 @@
-/*
-"Створіть веб-аплікацію, яка питає у користувача радіус кола та видає площу цього кола.
-
-Приклад:
-
---> 123 
-<-- 47529.15525615998
-
---> 3.09 
-<-- 29.996240815740702"
-
-попробывать консольный ввод данны и через форму
-*/
 function CalculateArea() {
 	let radius = document.getElementById('txtRadius').value;
 	document.getElementById("result").innerHTML = `Area = ${Math.pow(radius, 2) * Math.PI}`;
--- hw1/js/reverse.js
+++ hw1/js/reverse.js
@@ -1,16 +1,3 @@
-/*
-	"Запросіть у користувача трицифрове число і виведіть його задом наперед.
-
-Приклад:
-
---> 321
-<-- 123
-
---> 109
-<-- 901"
-*/
-
-
 function ReverseNum(num = document.getElementById('txtNumber').value) {
 	document.getElementById("result").innerHTML = num.split("").reverse().join("");
 }

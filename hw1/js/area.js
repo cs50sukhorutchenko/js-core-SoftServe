fa53ff9c024ddc57cd62ba24b2fb8b3df673a8bd
Volodymyr Sukhorutchenko <cs50sukhorutchenko@gmail.com>
2022-08-03 03:32:45 +0300

    Finish first task

---
+++ hw1/js/area.js
@@ -0,0 +1,17 @@
+/*
+"Створіть веб-аплікацію, яка питає у користувача радіус кола та видає площу цього кола.
+
+Приклад:
+
+--> 123 
+<-- 47529.15525615998
+
+--> 3.09 
+<-- 29.996240815740702"
+
+попробывать консольный ввод данны и через форму
+*/
+function CalculateArea() {
+	var radius = document.getElementById('txtRadius').value;
+	document.getElementById("result").innerHTML = `Area = ${radius * radius * Math.PI}`;
+}
